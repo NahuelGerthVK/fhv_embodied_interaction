060a505e15ab078e6611b5fde7f586413cb3691f
Nahuel Gerth <info@nahuelgerth.de>
2025-11-15 00:02:30 +0100

    blazepose: more detailed tracking model

---
+++ 3-ml5/2-pose/pose-5-detailed/sketch.js
@@ -0,0 +1,127 @@
+/* - - Variables - - */
+let bodyPose;
+let webcam;
+let poses = [];
+
+// smoothing
+let activateSmoothing = true;
+let smoothing = 0.4; // smaller = smoother
+
+/* - - Preload - - */
+function preload() {
+  bodyPose = ml5.bodyPose("BlazePose", {
+    enableSmoothing: true,
+    flipped: true,
+  });
+}
+
+/* - - Setup - - */
+function setup() {
+  createCanvas(1920, 1080); // 16:9
+
+  // scale factor
+  coverSketch();
+
+  // styling
+  noStroke();
+
+  // create webcam capture
+  webcam = createCapture({
+    video: { width: width, height: height },
+  });
+  webcam.hide();
+
+  // start tracking
+  bodyPose.detectStart(webcam, gotPoses);
+}
+
+/* - - Draw - - */
+function draw() {
+  background(0);
+
+  // webcam
+  image(webcam, 0, 0, width, height);
+
+  // 1. draw all keypoints
+
+  // 1.1  iterate through all detected people
+  for (let i = 0; i < poses.length; i++) {
+    let pose = poses[i];
+
+    // 1.2 iterate through all keypoints
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      let keypoint = pose.keypoints[j];
+
+      // confidence score
+      if (keypoint.confidence > 0.7) {
+        fill(255);
+        stroke(0);
+        strokeWeight(3);
+        ellipse(keypoint.x, keypoint.y, 10);
+      }
+    }
+  }
+
+  // 2. draw specific keypoints
+
+  // draw rectangle on nose
+  for (let i = 0; i < poses.length; i++) {
+    // let nose = poses[i].nose; // by name
+    let nose = poses[i].keypoints[0]; // by number
+
+    fill("red");
+    rectMode(CENTER);
+    rect(nose.x, nose.y, 20, 20);
+  }
+}
+
+/* - - Resize - - */
+function windowResized() {
+  coverSketch();
+}
+
+/* - - Functions - - */
+
+// pose tracking
+function gotPoses(results) {
+  // smoothing
+  if (activateSmoothing) {
+    if (poses.length > 0) {
+      for (let i = 0; i < results.length; i++) {
+        if (poses[i]) {
+          let oldPose = poses[i];
+          let newPose = results[i];
+          for (let j = 0; j < newPose.keypoints.length; j++) {
+            newPose.keypoints[j].x = lerp(
+              oldPose.keypoints[j].x,
+              newPose.keypoints[j].x,
+              smoothing
+            );
+            newPose.keypoints[j].y = lerp(
+              oldPose.keypoints[j].y,
+              newPose.keypoints[j].y,
+              smoothing
+            );
+          }
+        }
+      }
+    }
+  }
+
+  poses = results; // array of tracked points
+}
+
+// helper: cover sketch to window
+function coverSketch() {
+  let scale;
+
+  // get scale factors
+  let scaleX = windowWidth / width;
+  let scaleY = windowHeight / height;
+
+  // use the bigger scale
+  scale = max(scaleX, scaleY);
+
+  // apply scale
+  displayMode(NORMAL, SMOOTH, scale);
+}
